cb11410b54bdcd6bf15e843f30ee6ea58bcc0cee
postabarnabas <postabarnabas1017@gmail.com>
2025-12-01 13:30:10 +0100

    refactor: update Card class for view integration

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-
 public enum CardSuit
 {
     Piros,
@@ -8,6 +7,7 @@ public enum CardSuit
     Makk,
     Tok
 }
+
 public enum CardRank
 {
     Hetes = 7,
@@ -19,13 +19,39 @@ public enum CardRank
     Kiraly = 13,
     Asz = 14
 }
+
 public class Card
 {
     public CardSuit Suit { get; private set; }
     public CardRank Rank { get; private set; }
-    public Card (CardSuit suit, CardRank rank)
+
+    public Card(CardSuit suit, CardRank rank)
     {
         Suit = suit;
         Rank = rank;
+    }
+
+    public string GetCardFileName()
+    {
+        string suitName = Suit.ToString().ToLower();  
+        string rankName = RankToString();            
+
+        return $"{suitName}_{rankName}";
+    }
+
+    private string RankToString()
+    {
+        switch (Rank)
+        {
+            case CardRank.Hetes: return "hetes";
+            case CardRank.Nyolcas: return "nyolcas";
+            case CardRank.Kilences: return "kilences";
+            case CardRank.Tizes: return "tizes";
+            case CardRank.Also: return "also";   
+            case CardRank.Felso: return "felso";
+            case CardRank.Kiraly: return "kiraly";
+            case CardRank.Asz: return "asz";
+            default: return ((int)Rank).ToString();
+        }
     }
 }
